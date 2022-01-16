46f3520c3d8fafd5e371c9aeef99cf193ad00167
kotahorii <khorii.micron@gmail.com>
2022-01-16 23:22:50 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+      int y = Square(x);
+      Console.WriteLine(Square(x));
+    }
+    static int Square(int x)
+    {
+      int square = x * x;
+      return square;
+    }
+  }
+}
