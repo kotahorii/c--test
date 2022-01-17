8f855e9763b8444a86d174653b1af1d7b4308974
kotahorii <khorii.micron@gmail.com>
2022-01-18 01:05:34 +0900

    [Add] scripts

--- EnemyMove.cs
+++ EnemyMove.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMove : MonoBehaviour
 {
+  [SerializeField] private LayerMask raycastLayerMask;
   private NavMeshAgent _agent;
+  private RaycastHit[] _raycastHits = new RaycastHit[10];
   // Start is called before the first frame update
   void Start()
   {
@@ -17,7 +17,21 @@ public class EnemyMove : MonoBehaviour
   {
     if (collider.CompareTag("Player"))
     {
-      _agent.destination = collider.transform.position;
+      var positionDiff = collider.transform.position - transform.position;
+      var distance = positionDiff.magnitude;
+      var direction = positionDiff.normalized;
+
+      var hitCount = Physics.RaycastNonAlloc(transform.position, direction, _raycastHits, distance, raycastLayerMask);
+      Debug.Log("hitCount:" + hitCount);
+      if (hitCount == 0)
+      {
+        _agent.isStopped = false;
+        _agent.destination = collider.transform.position;
+      }
+      else
+      {
+        _agent.isStopped = true;
+      }
     }
   }
 }
